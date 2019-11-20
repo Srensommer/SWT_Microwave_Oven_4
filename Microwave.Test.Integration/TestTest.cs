e99ce7c6977d207bd20ec2500d2d9364b0295c50
srensommer <srensommer@gmail.com>
2019-11-20 14:02:49 +0100

    We ready to integrate!

---
+++ Microwave.Test.Integration/TestTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class TestTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            //uut = new Door();
+        }
+
+        [Test]
+        public void TestTestTest()
+        {
+
+        }
+    }
+}

af1aad4b8efca7253a9c6a74bdefc55f409e4e05
srensommer <srensommer@gmail.com>
2019-11-24 23:16:11 +0100

    IT step 1 lavet på 2 måder, jeg er usikker på, hvilken der er korrekt

--- Microwave.Test.Integration/IT01_PowerTubeWOutput.cs
+++ Microwave.Test.Integration/IT01_PowerTubeWOutput.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
@@ -10,17 +14,76 @@ namespace Microwave.Test.Integration
     [TestFixture]
     public class IT01_PowerTubeWOutput
     {
+        private IOutput output;
+        private PowerTube powerTube;
 
         [SetUp]
         public void Setup()
         {
-
+            //output = new Output();
+            output = Substitute.For<Output>();
+            powerTube = new PowerTube(output);
         }
 
         [Test]
-        public void TestTestTest()
+        public void TurnOn20Output20Test()
         {
+            string console;
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                powerTube.TurnOn(20);
+                console = stringWriter.ToString();
+            }
+            Assert.That(console, Is.EqualTo("PowerTube works with 20 %\r\n"));
+        }
+        [Test]
+        public void TurnOffOutputNothingTest()
+        {
+            string console;
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                powerTube.TurnOff();
+                console = stringWriter.ToString();
+            }
+            Assert.That(console, Is.EqualTo(""));
+        }
+        [Test]
+        public void TurnOn20TurnOffOutput20ThenTurnedOffTest()
+        {
+            string console;
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+                powerTube.TurnOn(20);
+                powerTube.TurnOff();
+                console = stringWriter.ToString();
+            }
+            Assert.That(console, Is.EqualTo("PowerTube works with 20 %\r\nPowerTube turned off\r\n"));
+        }
 
+
+
+        //TODO: Denne og de to næste tests melder tilbage uanset input, at testen er gået godt...
+        [Test]
+        public void TurnOn30Output30Test()
+        {
+            //TODO: Bruger stubben pt.. Burde vi bruge et "rigtigt" output, som ovenover??
+            powerTube.TurnOn(30);
+            output.Received().OutputLine("PowerTube works with 30 %\r\n");
+        }
+        [Test]
+        public void TurnOffOutputNothingTest2()
+        {
+            powerTube.TurnOff();
+            output.Received().OutputLine("");
+        }
+        [Test]
+        public void TurnOn30TurnOffOutputTest()
+        {
+            powerTube.TurnOn(30);
+            powerTube.TurnOff();
+            output.Received().OutputLine("PowerTube works with 30 %\r\nPowerTube turned off\r\n");
         }
     }
 }

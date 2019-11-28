217fb16a84d5972274b13fe5c006e18b4bad0129
srensommer <srensommer@gmail.com>
2019-11-28 14:29:43 +0100

    4 og 5 burde være færdige. 6 virker dog ikke rigtigt

--- Microwave.Test.Integration/IT06_CookControllerWTimer.cs
+++ Microwave.Test.Integration/IT06_CookControllerWTimer.cs
@@ -33,22 +33,30 @@ namespace Microwave.Test.Integration
         [Test]
         public void CookingControllerGetsTickEventAfter1Second()
         {
+            //Vi bruger her ManualResetEvent. Vi er inspirerede af https://docs.microsoft.com/en-us/dotnet/api/system.threading.manualresetevent?view=netframework-4.8
             ManualResetEvent pause = new ManualResetEvent(false);
             timer.TimerTick += (timer, tick) => pause.Set();
             cookController.StartCooking(50, 2);
             //Venter til vi får et tick, og tjekker derefter at vi rigtig nok har modtaget et event.
             Assert.AreEqual(true, pause.WaitOne(1100));
-
         }
         [Test]
-        public void CookingControllerGetsNoTickEvent()
+        public void CookingControllerGetsNoTickEventAfter1MilliSecond()
         {
             ManualResetEvent pause = new ManualResetEvent(false);
             timer.TimerTick += (timer, tick) => pause.Set();
             cookController.StartCooking(50, 2);
             //Venter, men ikke lang nok tid til vi får et tick, og tjekker derefter at vi rigtig nok ikke har modtaget et event.
             Assert.AreEqual(false, pause.WaitOne(1));
-
+        }
+        [Test]
+        public void CookingControllerGetsTimeExpiredEventAfter1Second()
+        {
+            ManualResetEvent pause = new ManualResetEvent(false);
+            timer.Expired += (timer, tick) => pause.Set();
+            cookController.StartCooking(50, 1);
+            //Venter, lang nok tid til vi får et tick, dette tick er det sidste, så derfor forventer vi et time expired event
+            Assert.AreEqual(true, pause.WaitOne(2000));
         }
     }
 }
